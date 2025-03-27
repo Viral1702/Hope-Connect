c525aaf00209a1cfc7d4ae7d57e4b15e71e66df0
Priyal-Bhesaniya <priyalbhesaniya@gmail.com>
2025-03-27 09:35:27 +0530

    give components to login

--- Frontend/src/Pages/Auth/Loginpage.jsx
+++ Frontend/src/Pages/Auth/Loginpage.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import LoginForm from "../../components/user/login/LoginForm";
 
 const Loginpage = () => {
   return (
     <div>
       Login <NavLink to="/">Home</NavLink>
+      <UserHeader/>
+      <LoginForm/>
     </div>
   );
 };
