684485d090cd9709c0d8b736892758d32a610fc9
Madhvi <madhvisan5277@gmail.com>
2025-03-28 10:56:20 +0530

    conflict resolve

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -24,18 +24,13 @@ const App = () => {
           <Route path="/aboutus" element={<AboutUs />} />
         </Route>
         // User Routes // Auth Routes
-<<<<<<< HEAD
-          <Route path="/user" element={<UserOutlet />}>
-          <Route path="login" element={<Loginpage />} />
-=======
+
         <Route path="/login" element={<LoginOutlet />}>
           <Route index element={<Loginpage />} /> {/* Default page inside /login */}
         </Route>
         
         <Route path="/user" element={<UserOutlet />}>
-          
-          
->>>>>>> 7232a20def72da5bc129560314ff54ca18475bb3
+        
         </Route>
         // Organization Routes
         <Route path="/organization" element={<OrganizationOutlet />}>
